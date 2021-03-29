97e0345637ac368fb5ef2e63432da8fd30133a20
JaxtonHale <theslammer100@gmail.com>
2021-03-29 16:56:41 -0400

    testing maybe broken

--- packages.el
+++ packages.el
@@ -1,4 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
+<<<<<<< HEAD
 ;;; $DOOMDIR/packages.el
 
 ;; To install a package with Doom you must declare them here and run 'doom sync'
@@ -50,3 +51,13 @@
 ;(unpin! t)
 
 (package! evil-tutor)
+=======
+;;; config/default/packages.el
+
+(package! avy :pin "bbf1e7339eba06784dfe86643bb0fbddf5bb0342")
+(package! drag-stuff :pin "6d06d846cd37c052d79acd0f372c13006aa7e7c8")
+(package! link-hint :pin "e897897d6a92b4ffdbff33acf69f5c661da1123a")
+
+(unless (featurep! :editor evil)
+  (package! expand-region :pin "ea6b4cbb9985ddae532bd2faf9bb00570c9f2781"))
+>>>>>>> 7d4ff97 (new update?? perhaps broken we'll see)
